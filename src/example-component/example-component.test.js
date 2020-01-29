9dea30e7de57ad2bd743fc5aeb1c15732249ccb9
adriano.rodrigues <adriano.rodrigues@wooza.com.br>
2020-01-29 15:37:34 -0300

    fix(example-component.test.js): adjust imports

--- src/example-component/example-component.test.js
+++ src/example-component/example-component.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ExampleComponent from '.';
+import ExampleComponent from './example-component';
 
 describe('<ExampleComponent />', () => {
   test('deve estar declarado', () => {
